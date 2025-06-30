d9ae4d7911bf1c82e1f3a3216f604df77723fe83
MaoEr <zevixquatol@gmail.com>
2025-06-30 14:37:12 +0800

    页面搭建

--- components/HomeBar.vue
+++ components/HomeBar.vue
@@ -1,4 +1,7 @@
 <template>
+	<view class="bar-view">
+		
+	</view>
 </template>
 
 <script>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,15 +1,33 @@
 <template>
 	<view class="index-view">
 		<view class="index-card">
-			<view class="index-card-title">C</view>
-			<view class="index-card-title">J</view>
-			<view class="index-card-title">D</view>
-			<view class="index-card-title">L</view>
-			<view class="index-card-title">T</view>
+			<view class="index-card-head">
+				<text class="index-card-title">超级大乐透</text>
+				<!-- <wd-switch v-model="dltSwitch" active-color="#3D5A80" inactive-color="#0F1C2E"></wd-switch> -->
+				<view class="z-btn">
+					<text>选号</text>
+					<wd-icon name="arrow-right" size="18px"></wd-icon>
+				</view>
+			</view>
+			<view class="index-card-content" v-if="dltSwitch">
+				<view class="draw-num">
+					<view class="font-num">01 02 03 04 05</view>
+					<view class="back-num">06 07</view>
+				</view>
+				<view class="draw-date">
+					<text>2025140期</text>
+					<text>2025-01-01 08:00:00</text>
+				</view>
+			</view>
 		</view>
 	</view>
 </template>
 
-<script setup></script>
+<script setup>
+import { onLoad, onShow } from '@dcloudio/uni-app';
+import { ref } from 'vue';
+
+const dltSwitch = ref(true);
+</script>
 
 <style></style>
